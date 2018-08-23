69601fbf98ebf2c98785a0cec0c4e616a686e0fe
Jaka Stavanja <jakastavanja@gmail.com>
2018-08-23 20:17:53 +0200

    Added face comparison recording result model and view logic

--- face/models.py
+++ face/models.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from student.models import Student
+from quiz.models import Quiz
 
 # Create your models here.
+class FaceComparisonResult(models.Model):
+  quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+  student = models.ForeignKey(Student, on_delete=models.CASCADE)
+  distance = models.FloatField()
+
+  def __str__(self):
+    return "Student: " + str(self.student) + " @ " + str(self.quiz_id) + ", Distance: " + str(self.distance)
--- face/views.py
+++ face/views.py
@@ -10,19 +10,27 @@ import json
 
 from student.models import Student
 from .detector import Detector
+from .models import FaceComparisonResult
+from quiz.models import Quiz
 
 class FaceDistanceAPIView(generics.ListAPIView):
 
   def post(self, request, *args, **kwargs):
 
+    # dont do anything if user already has submitted this test 
     user = request.POST.get('user_id')
-    face_image_path = Student.objects.filter(moodle_username=user)[0].face_image.path
+    student = Student.objects.filter(moodle_username=user)[0]
+    face_image_path = student.face_image.path
     current_image_path = request.FILES['current_image'].temporary_file_path()
-    test_type_id = request.POST.get('test_type')
+    quiz = Quiz.objects.filter(id=request.POST.get('quiz_id'))[0]
 
     d = Detector(face_image_path, current_image_path)
+    distance = d.get_distance()
+
+    result = FaceComparisonResult(quiz_id = quiz,student = student, distance = distance)
+    result.save()
     
     return Response({
       'moodle_username': user,
-      'distance': d.get_distance()
+      'distance': distance
     })
